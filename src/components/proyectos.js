598fe83289e221ca715f3ddcdb134a4b16370fce
Martinmessner <martinmessner95@gmail.com>
2024-01-16 18:10:11 -0300

    modify desing styles web

---
+++ src/components/proyectos.js
@@ -0,0 +1,106 @@
+const PROYECTOS = [
+    {
+        imagen:"/btc.webp",
+        title_alt:"bitcoin",
+        title:"App de Bitcoin | WebSockets React",
+          rel:"noopener noreferrer",
+          url :"https://cryptocodebtc.netlify.app/",
+          github: "https://github.com/Martinmessner/WebSocket-React-BTC"
+    }, //listo
+    { 
+        imagen:"/busqueda-img.webp", 
+        title_alt:"buscador-de-imagenes",
+        title:"Buscador de Imagenes",
+          rel:"noopener noreferrer",
+          url :"https://imagenhd-react.netlify.app/",
+          github: "https://github.com/Martinmessner/buscadorimagenes-react"
+    }, // listo
+    {
+        imagen:"/btc.webp",
+        title_alt:"bitcoin",
+        title:"bitcoin-app",
+          rel:"noopener noreferrer",
+          url :"https://cryptocodebtc.netlify.app/",
+          github: "https://github.com/Martinmessner/WebSocket-React-BTC"
+    },
+    {
+        imagen:"/btc.webp",
+        title_alt:"bitcoin",
+        title:"bitcoin-app",
+          rel:"noopener noreferrer",
+          url :"https://cryptocodebtc.netlify.app/",
+          github: "https://github.com/Martinmessner/WebSocket-React-BTC"
+    },
+    {
+        imagen:"/btc.webp",
+        title_alt:"bitcoin",
+        title:"bitcoin-app",
+          rel:"noopener noreferrer",
+          url :"https://cryptocodebtc.netlify.app/",
+          github: "https://github.com/Martinmessner/WebSocket-React-BTC"
+    },
+    {
+        imagen:"/btc.webp",
+        title_alt:"bitcoin",
+        title:"bitcoin-app",
+          rel:"noopener noreferrer",
+          url :"https://cryptocodebtc.netlify.app/",
+          github: "https://github.com/Martinmessner/WebSocket-React-BTC"
+    },
+    {
+        imagen:"/btc.webp",
+        title_alt:"bitcoin",
+        title:"bitcoin-app",
+          rel:"noopener noreferrer",
+          url :"https://cryptocodebtc.netlify.app/",
+          github: "https://github.com/Martinmessner/WebSocket-React-BTC"
+    },
+    {
+        imagen:"/btc.webp",
+        title_alt:"bitcoin",
+        title:"bitcoin-app",
+          rel:"noopener noreferrer",
+          url :"https://cryptocodebtc.netlify.app/",
+          github: "https://github.com/Martinmessner/WebSocket-React-BTC"
+    },
+    {
+        imagen:"/btc.webp",
+        title_alt:"bitcoin",
+        title:"bitcoin-app",
+          rel:"noopener noreferrer",
+          url :"https://cryptocodebtc.netlify.app/",
+          github: "https://github.com/Martinmessner/WebSocket-React-BTC"
+    },
+    {
+        imagen:"/btc.webp",
+        title_alt:"bitcoin",
+        title:"bitcoin-app",
+          rel:"noopener noreferrer",
+          url :"https://cryptocodebtc.netlify.app/",
+          github: "https://github.com/Martinmessner/WebSocket-React-BTC"
+    },
+    {
+        imagen:"/btc.webp",
+        title_alt:"bitcoin",
+        title:"bitcoin-app",
+          rel:"noopener noreferrer",
+          url :"https://cryptocodebtc.netlify.app/",
+          github: "https://github.com/Martinmessner/WebSocket-React-BTC"
+    },
+    {
+        imagen:"/btc.webp",
+        title_alt:"bitcoin",
+        title:"bitcoin-app",
+          rel:"noopener noreferrer",
+          url :"https://cryptocodebtc.netlify.app/",
+          github: "https://github.com/Martinmessner/WebSocket-React-BTC"
+    },
+    {
+        imagen:"/btc.webp",
+        title_alt:"bitcoin",
+        title:"bitcoin-app",
+          rel:"noopener noreferrer",
+          url :"https://cryptocodebtc.netlify.app/",
+          github: "https://github.com/Martinmessner/WebSocket-React-BTC"
+    }
+]
