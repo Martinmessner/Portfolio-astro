faf3fd77193fc9dc96e80c715d2b6efbe5005df3
Martinmessner <martinmessner95@gmail.com>
2024-01-17 20:50:00 -0300

    add astro skills

--- src/components/proyectos.js
+++ src/components/proyectos.js
@@ -1,82 +1,86 @@
 export const PROYECTOS = [
     {
+        imagen:"/lol-img.webp",
+        title_alt:"League Of Legends",
+        title:"Buscador de Usuarios de Lol",
+        url : "https://lol-metrics.netlify.app/",
+        github: "https://github.com/Martinmessner/league-of-legends"
+    }, 
+    {
         imagen:"/btc.webp",
         title_alt:"bitcoin",
         title:"App de Bitcoin | WebSockets React",
-          rel:"noopener noreferrer",
-          url :"https://cryptocodebtc.netlify.app/",
-          github: "https://github.com/Martinmessner/WebSocket-React-BTC"
-    }, //listo
+        url :"https://cryptocodebtc.netlify.app/",
+        github: "https://github.com/Martinmessner/WebSocket-React-BTC"
+    }, 
     { 
         imagen:"/busqueda-img.webp", 
         title_alt:"buscador-de-imagenes",
         title:"Buscador de Imagenes",
-          rel:"noopener noreferrer",
-          url :"https://imagenhd-react.netlify.app/",
-          github: "https://github.com/Martinmessner/buscadorimagenes-react"
-    }, // listo
+        url :"https://imagenhd-react.netlify.app/",
+        github: "https://github.com/Martinmessner/buscadorimagenes-react"
+    }, 
     {
         imagen:"/chatgpt.PNG",
         title_alt:"ChatGpt",
-        title:"ChatGpt Express | React",
-          rel:"noopener noreferrer",
-          url :"https://chatgpt-messner.netlify.app/",
-          github: "https://github.com/Martinmessner/messnerIA-openAI"
-    }, // listo
-    {
-        imagen:"/ecommerce2.PNG",
-        title_alt:"Ecommerce",
-        title:"Ecommerce | HTML, CSS, JavaScript, Node, Express , MongoDB , MercadoPago",
-          rel:"noopener noreferrer",
-          url :"",
-          github: "https://github.com/Martinmessner/API-REST-Ecommerce-NodeJs"
-    }, // listo
+        title:"ChatGpt Express | React",      
+        url :"https://chatgpt-messner.netlify.app/",
+        github: "https://github.com/Martinmessner/messnerIA-openAI"
+    }, 
+   
     {
         imagen:"/paisesReact.PNG",
         title_alt:"Paises",
         title:"Paises con React & React-draggable",
-          rel:"noopener noreferrer",
-          url :"https://allpaises.netlify.app/",
-          github: "https://github.com/Martinmessner/React-Paises"
-    }, // listo
+        url :"https://allpaises.netlify.app/",
+        github: "https://github.com/Martinmessner/React-Paises"
+    }, 
     {
         imagen:"/crudreact.PNG",
         title_alt:"Lista de Tareas",
         title:"App de Notas | React",
-          rel:"noopener noreferrer",
+        
           url :"https://notas-efectosublimados.netlify.app/",
           github: "https://github.com/Martinmessner/CRUDREACT-NOTAS"
-    }, // listo
+    }, 
     {
         imagen:"/navideño.PNG",
         title_alt:"Lista navideña | GoncyPozzo",
         title:"App de Regalos | React",
-          rel:"noopener noreferrer",
+        
           url :"https://regalos-navidenios.netlify.app/",
           github: "https://github.com/Martinmessner/challenge-goncy-advency"
-    }, // listo
+    }, 
     {
         imagen:"/entrevista-tecnica.webp",
         title_alt:"Entrevista Tecnica | Midudev",
         title:"Entrevista Tecnica | Midudev",
-          rel:"noopener noreferrer",
+        
           url :"https://practica-de-entrevista-tenica.netlify.app/",
           github: "https://github.com/Martinmessner/entrevista-tecnica-react-midudev-prueba"
-    }, // listo
+    }, 
     {
         imagen:"/ta-te-ti.webp",
         title_alt:"Ta-Te-Ti",
         title:"Ta-Te-Ti | React",
-          rel:"noopener noreferrer",
+        
           url :"https://tateti-messi.netlify.app/",
           github: "https://github.com/Martinmessner/tateti-practica-react-vite"
-    }, // listo
+    }, 
+    {
+      imagen:"/ecommerce2.PNG",
+      title_alt:"Ecommerce",
+      title:"Ecommerce | JavaScript, Node, Express, MongoDB, MercadoPago",
+       
+        url : "",
+        github: "https://github.com/Martinmessner/API-REST-Ecommerce-NodeJs"
+  }, 
     {
       imagen:"/frontendmentor-landingpage.PNG",
       title_alt:"Frontend-Mentor",
       title:"Challenge Frontend Mentor | Clon HTML - CSS - JavaScript",
-        rel:"noopener noreferrer",
+       
         url :"https://landin-page-frontendmentor.netlify.app/",
         github: "https://github.com/Martinmessner/FRONTENDMENTOR-LandingPage"
-  }, // listo
+  }, 
 ]
