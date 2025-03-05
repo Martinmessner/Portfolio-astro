aaf1af79428d731c5beba3da60bd7c63668df553
Martinmessner <martinmessner95@gmail.com>
2025-03-05 18:34:17 -0300

    add change

---
+++ src/components/proyectos.js
@@ -0,0 +1,125 @@
+export const PROYECTOS = [
+    {
+        imagen:"/lol-img.webp",
+        title_alt:"League Of Legends",
+        title:"Buscador de Usuarios de Lol",
+        url : "https://lol-metrics.netlify.app/",
+        github: "https://github.com/Martinmessner/league-of-legends"
+    }, 
+    {
+        imagen:"/btc.webp",
+        title_alt:"bitcoin",
+        title:"App de Bitcoin | WebSockets React",
+        url :"https://cryptocodebtc.netlify.app/",
+        github: "https://github.com/Martinmessner/WebSocket-React-BTC"
+    }, 
+    { 
+        imagen:"/busqueda-img.webp", 
+        title_alt:"buscador-de-imagenes",
+        title:"Buscador de Imagenes",
+        url :"https://imagenhd-react.netlify.app/",
+        github: "https://github.com/Martinmessner/buscadorimagenes-react"
+    }, 
+    {
+      imagen:"/crudreact.webp",
+      title_alt:"Lista de Tareas",
+      title:"App de Notas | React",
+      url :"https://notas-efectosublimados.netlify.app/",
+      github: "https://github.com/Martinmessner/CRUDREACT-NOTAS"
+    }, 
+    {
+        imagen:"/chatgpt.PNG",
+        title_alt:"ChatGpt",
+        title:"ChatGpt Express | React",      
+        url :"",
+        github: "https://github.com/Martinmessner/messnerIA-openAI"
+    }, 
+   
+    {
+      imagen:"/ta-te-ti.webp",
+      title_alt:"Ta-Te-Ti",
+      title:"Ta-Te-Ti | React",
+      url :"https://tateti-messi.netlify.app/",
+      github: "https://github.com/Martinmessner/tateti-practica-react-vite"
+    }, 
+    {
+      imagen:"/ecommerce2.PNG",
+      title_alt:"Ecommerce",
+      title:"Ecommerce | JavaScript, Node, Express, MongoDB, MercadoPago",
+      url : "",
+      github: "https://github.com/Martinmessner/API-REST-Ecommerce-NodeJs"
+  },
+  {
+    imagen:"/entrevista-tecnica.webp",
+    title_alt:"Entrevista Tecnica | Midudev",
+    title:"Entrevista Tecnica | Midudev",
+    url :"https://practica-de-entrevista-tenica.netlify.app/",
+    github: "https://github.com/Martinmessner/entrevista-tecnica-react-midudev-prueba"
+  }, 
+  {
+    imagen:"/navideño.PNG",
+    title_alt:"Lista navideña | GoncyPozzo",
+    title:"App de Regalos | React",
+    url :"https://regalos-navidenios.netlify.app/",
+    github: "https://github.com/Martinmessner/challenge-goncy-advency"
+  }, 
+    {
+        imagen:"/paisesReact.PNG",
+        title_alt:"Paises",
+        title:"Paises con React & React-draggable",
+        url :"https://allpaises.netlify.app/",
+        github: "https://github.com/Martinmessner/React-Paises"
+    }
+]
+export const SKILLS = [
+  {
+    alt: "HTML",
+    title: "HTML",
+    src: "/htmll.svg"
+  },
+  {
+    alt: "CSS",
+    title: "CSS",
+    src: "/css.svg"
+  },
+  {
+    alt: "JavaScript",
+    title: "JavaScript",
+    src: "/jss.svg"
+  },
+  {
+    alt: "TypeScript",
+    title: "TypeScript",
+    src: "/tsc.png"
+  },
+  {
+    alt: "React",
+    title: "React",
+    src: "/react.png"
+  },
+  {
+    alt: "Node",
+    title: "Node",
+    src: "/node.webp"
+  },
+  {
+    alt: "Astro",
+    title: "Astro",
+    src: "/astro.webp"
+  },
+  {
+    alt: "Git",
+    title: "Git",
+    src: "/git.svg"
+  },
+  {
+    alt: "GitHub",
+    title: "GitHub",
+    src: "/github2.svg"
+  },
+  {
+    alt: "MongoDB",
+    title: "MongoDB",
+    src: "/mongodb-ar21.svg"
+  }
+];
